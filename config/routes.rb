efada12abe1ca37e24980e82d5634a08f4876dea
shahminrazali <shahminrazali@gmail.com>
2016-08-03 17:55:50 +0800

    Day 3 end

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'landing#index'
   get :about , to:'static_pages#about'
+  resources :topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

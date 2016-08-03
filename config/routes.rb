cca48b0e0daac0f2de494b8818a535e4a50e8b05
shahminrazali <shahminrazali@gmail.com>
2016-08-03 13:06:11 +0800

    About Page Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'landing#index'
+  get :about , to:'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
